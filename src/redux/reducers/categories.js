0670fd343cc4ec6a22e016017bc68c58b23e4e88
Anderson Barrera <andersonbar10@gmail.com>
2023-11-08 10:48:40 -0500

    payload de categories

--- src/redux/reducers/categories.js
+++ src/redux/reducers/categories.js
@@ -8,7 +8,7 @@ const initialState = {
 };
 
 export default function Categories(state = initialState, action) {
-    const { type, payload } = action;
+    const { type, payload } =  console.log(`${process.env.REACT_APP_API_URL}/api/category/categories`);
 
     switch(type) {
         case GET_CATEGORIES_SUCCESS:
