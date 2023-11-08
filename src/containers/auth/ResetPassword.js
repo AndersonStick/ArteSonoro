9f8f5b243df41765a2f9b82c29b525e6b2629bb1
Anderson Barrera <andersonbar10@gmail.com>
2023-11-08 18:50:13 -0500

    Cambio de example por ejemplo

--- src/containers/auth/ResetPassword.js
+++ src/containers/auth/ResetPassword.js
@@ -63,7 +63,7 @@ const ResetPassword = ({
                     value={email}
                     onChange={e=>onChange(e)}
                     type="email"
-                    placeholder="example@mail.com"
+                    placeholder="ejemplo@mail.com"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-custom-blue focus:border-custom-blue sm:text-sm"
                   />
