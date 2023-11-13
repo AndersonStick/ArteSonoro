70d5c240fbabe8d993d50ccd148fe89168483748
Anderson Barrera <andersonbar10@gmail.com>
2023-11-13 12:12:07 -0500

    Se cambia hora para intentar hacer traza de user

--- src/redux/actions/alert.js
+++ src/redux/actions/alert.js
@@ -2,7 +2,7 @@ import {
     SET_ALERT,
     REMOVE_ALERT,
 } from './types';  
-               
+                
    
 export const setAlert = (msg, alertColor, timeout = 5000) => dispatch => {
     dispatch({
