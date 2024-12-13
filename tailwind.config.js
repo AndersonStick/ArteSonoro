93945607d4ce1b956ee5a1eea789fd089d2e07e0
Anderson Barrera <andersonbar10@gmail.com>
2024-12-13 11:25:46 -0500

    Cambio de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'custom-blue': '#144383',       // Color azul main
-        'custom-hover-blue': '#749cbc', // Color al tener el mouse encima de un boton azul
+        'custom-blue': '#f29325',       // Color azul main
+        'custom-hover-blue': '#007172', // Color al tener el mouse encima de un boton azul
         'custom-gris-uno': '#7c7c7c', // Gris 
         'custom-gris-dos': '#84847c', // Gris 2
 
